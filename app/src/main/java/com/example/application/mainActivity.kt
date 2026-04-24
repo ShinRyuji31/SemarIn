6e626d567ccdba70e1f15f0d10d977e5f35e2a59
Eypril <liawulan038@gmail.com>
2026-04-24 10:26:46 +0700

    hapus yg gk penting
    Removed comments related to navigation stack initialization and functionality.

--- app/src/main/java/com/example/application/mainActivity.kt
+++ app/src/main/java/com/example/application/mainActivity.kt
@@ -16,22 +16,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             ApplicationTheme {
 
-                // 1. Inisialisasi BackStack (Tumpukan Layar) Navigasi 3
-                // Kita mulai tumpukan pertamanya dengan halaman LandingRoute
                 val backStack = remember { mutableStateListOf<Routes>(Routes.LandingRoute) }
-
-                // 2. Mengambil layar paling atas (terakhir ditambahkan) untuk ditampilkan
                 val currentRoute = backStack.last()
-
-                // 3. Memanggil file Compositions.kt (AppNavigation)
+                
                 AppNavigation(
                     currentRoute = currentRoute,
                     onNavigate = { newRoute ->
-                        // Misi: Basic Routing (Pindah ke halaman baru dengan cara ditumpuk)
                         backStack.add(newRoute)
                     },
                     onBack = {
-                        // Misi: Back Navigation (Mengambil/menghapus tumpukan teratas tanpa error)
                         if (backStack.size > 1) {
                             backStack.removeLastOrNull()
                         }
